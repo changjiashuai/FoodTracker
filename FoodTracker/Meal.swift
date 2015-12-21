2df45571005f250dd3b42d574b0704dde91cd742
changjiashuai <changjiashuai@gmail.com>
2015-12-22 00:38:02 +0800

    step-7 finish with XCTest

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,30 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by CJS on 15/12/22.
+//  Copyright © 2015年 CJS. All rights reserved.
+//
+
+import UIKit
+
+class Meal{
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int){
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+}
